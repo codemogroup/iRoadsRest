5600e728a76dbbcce6ec99940e5c8a19470a3ade
dushan14 <dushan.14@cse.mrt.ac.lk>
2018-04-23 22:06:59 +0530

    initial commit

---
+++ src/main/java/com/codemo/iroads/IroadsApplication.java
@@ -0,0 +1,12 @@
+package com.codemo.iroads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IroadsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codemo/iroads/IroadsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codemo.iroads;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IroadsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
