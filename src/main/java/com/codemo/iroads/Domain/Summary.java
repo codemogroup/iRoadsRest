865035b0c2e560f8869c0da307950badcbd7f443
Dushan Kumarasinghe <dushan.14@cse.mrt.ac.lk>
2018-10-05 00:44:12 +0530

    added summary

---
+++ src/main/java/com/codemo/iroads/Domain/Summary.java
@@ -0,0 +1,23 @@
+package com.codemo.iroads.Domain;
+
+public class Summary {
+
+    private int journeyCount;
+    private int taggedJourneyCount;
+
+    public int getJourneyCount() {
+        return journeyCount;
+    }
+
+    public void setJourneyCount(int journeyCount) {
+        this.journeyCount = journeyCount;
+    }
+
+    public int getTaggedJourneyCount() {
+        return taggedJourneyCount;
+    }
+
+    public void setTaggedJourneyCount(int taggedJourneyCount) {
+        this.taggedJourneyCount = taggedJourneyCount;
+    }
+}
